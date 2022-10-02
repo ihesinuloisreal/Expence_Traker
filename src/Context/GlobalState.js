75694ecbc51153e90d58e2e0f3cde5a073803bab
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2022-10-02 14:13:36 -0700

    context 1

---
+++ src/Context/GlobalState.js
@@ -0,0 +1,9 @@
+import React, {createContext, useReducer } from "react";
+
+// Initial state
+const initialState = {
+    transactions: [
+        {id: 1, text: 'Flower', amount: -20 },
+        
+    ]
+}
